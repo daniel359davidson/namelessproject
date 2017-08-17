b95fac30dba8c86998347018c1eba70689255200
VladislavHacker <vladislavhacker111@gmail.com>
2017-08-17 11:23:19 +0300

    server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+//http
+var http = require('http');
+//ws
+var ws =  new require('ws');
+//mysql
+var mysql = require('mysql');
+//connected clients
+var clients = new Array();
+
+var wsServer = new ws.Server({port:8081})
+
+wsServer.on('conection',function(ws){
+     conectionID = Math.random();
+     clients[conectionID] = ws;
+     console.log("новое соединение " + conect_id);
+})
+
+var httpServer =   http.createServer(function(res,req){
+     res.end();
+}).listen(8080);
+console.log("Сервер запущен на портах 8080, 8081");
